d8ec15c947b9335f061a6d40b59a7209587491b1
VandruGG <andru4@gmail.com>
2023-04-26 22:32:21 -0300

    Primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,25 @@
+
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Commodo veniam sint in do id Lorem pariatur eu Lorem cillum consectetur Lorem. Et anim voluptate nulla tempor enim culpa voluptate ut adipisicing. Velit elit eiusmod culpa duis cupidatat sit.',
+            picture: null,            
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'Consectetur esse reprehenderit mollit magna qui consequat occaecat consectetur et ex reprehenderit sit. Amet sint dolor aliquip ut quis non. Laborum eiusmod occaecat culpa cupidatat commodo mollit excepteur ad culpa ut labore sit ullamco aute. Ea mollit elit eiusmod excepteur.',
+            picture: null,            
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'Culpa reprehenderit incididunt sint ipsum ut. Reprehenderit nostrud sint ea labore ullamco tempor anim anim amet sit laborum et laboris minim. Eu tempor deserunt incididunt enim incididunt occaecat ullamco et excepteur ipsum eiusmod. Aliquip consectetur cillum incididunt tempor nulla irure consequat consectetur voluptate ex. Amet qui culpa labore cillum cupidatat. Culpa ullamco magna cillum laborum fugiat.',
+            picture: null,            
+        }
+    ]
+})
+
